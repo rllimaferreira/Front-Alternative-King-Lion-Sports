67a90a20ba5168813e73d2ec9e3f6aa8de17d4b3
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-09-20 15:42:13 -0300

    Carrossel, ErrorPG, about and Card

--- src/NavAndSid_Bar.jsx
+++ src/NavAndSid_Bar.jsx
@@ -7,7 +7,7 @@ function NavAndSid_Bar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">DH-Sports-Ecommerce</Navbar.Brand>
+        <Navbar.Brand >DH-Sports-Ecommerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -15,6 +15,8 @@ function NavAndSid_Bar() {
             <Link className="nav-link" to="/pg_admin">Admin</Link>
             <Link className="nav-link" to="/pg_products">Products</Link>
             <Link className="nav-link" to="/pg_categories">Categories</Link>
+            <Link className="nav-link" to="/pg_carrinho">Carrinho</Link>
+            <Link className="nav-link" to="/pg_sobre">Sobre nós</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
