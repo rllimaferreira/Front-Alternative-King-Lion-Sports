a9d7dc1a6ee9471d8204d01c04876070c3d15391
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-09-21 19:50:43 -0300

    Finished footer, without the deafault .ico

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,46 @@
-import { BsFacebook,BsInstagram,BsYoutube } from "react-icons/bs";
-
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="CorNav w-100">
-    
-        <div> teste</div>
+    <footer className="CorNav w-100 d-flex flex-column align-items-center text-center">
+      <div className="navFooter p-1">
+        <Link className="navgationItem nav-link textStyleFooter" to="/">
+          Home
+        </Link>
 
-        <BsFacebook/> <BsInstagram/> <BsYoutube/>
-      
-      
+        <Link className="navgationItem  nav-link textStyleFooter" to="/pg_products">
+          Products
+        </Link>
+        <Link className="navgationItem  nav-link textStyleFooter" to="/pg_categories">
+          Categories
+        </Link>
+        <Link className="navgationItem  nav-link textStyleFooter" to="/pg_sobre">
+          About Us
+        </Link>
+      </div>
+      <spam className="lineBreakSmall" />
+      <p className="textStyleFooter">Social Mídia</p>
+     
+      <div className="SocialMidia d-flex flex-row">
+        <Link className="socialIco nav-link" to="#">
+          <BsFacebook size={32}/>
+        </Link>
+        <Link className="socialIco nav-link" to="#">
+          <BsInstagram size={32} />
+        </Link>
+        <Link className="socialIco nav-link" to="#">
+          <BsYoutube size={32}/>
+        </Link>
+      </div>
+
+      <spam className="lineBreakBig" />
+      <p className="copyRightText">
+        Prices, promotions, payment terms, shipping and products are valid exclusively for purchases made via the internet. 
+        Photos for illustrative purposes only. 
+        <br/><br/>
+        Copyright © 2022 All rights reserved King Lion Ecommerce</p>
     </footer>
   );
 }
-
-
