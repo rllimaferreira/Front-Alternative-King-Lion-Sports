64b45cb54eeafe8d21797d4453b68aa029baeaab
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-09-21 16:33:17 -0300

    corNav, iconCar

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -1,10 +1,12 @@
 import 'react-bootstrap/'
 import Container from "react-bootstrap/Container"
 import Carousel from 'react-bootstrap/Carousel';
+
 import img_0 from '../imgs/wallpaperflare.com_wallpaper.jpg'
 import img_1 from '../imgs/wallpaperflare.com_wallpaper(1).jpg'
 import img_3 from '../imgs/wallpaperflare.com_wallpaper(3).jpg'
 import img_4 from '../imgs/wallpaperflare.com_wallpaper(4).jpg'
+
 import './Home.css'
 
 export default function Home() {
@@ -20,11 +22,6 @@ export default function Home() {
         <Carousel.Caption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-        <img className="d-block w-100 crs_img" src={img_1} alt="First slide"/>
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
 
