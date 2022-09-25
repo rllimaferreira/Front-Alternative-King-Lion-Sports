a6a46b9a0e1f3437c372c341d54feac5cc9c43d5
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-09-25 02:03:51 -0300

    Update Carrinho.jsx

--- src/pages/carrinho_pg/Carrinho.jsx
+++ src/pages/carrinho_pg/Carrinho.jsx
@@ -3,11 +3,12 @@ import { Card, Button, Container } from "react-bootstrap/";
 import { Link } from "react-router-dom";
 import EmptyCart from "../../Components/EmptyCart";
 
+
 export default function Carrinho() {
   return (
     <>
       <Container className="mt-3 mb-3 d-flex flex-column align-items-center">
-        <div className="buyingSteps  d-flex flex-row">
+        <div className="buyingSteps d-none d-md-flex d-xl-flex">
           <div className="steppCarrinho d-flex flex-column align-items-center">
             <svg
               width="36"
@@ -151,7 +152,7 @@ export default function Carrinho() {
               width="20"
               height="20"
               fill="currentColor"
-              class="bi bi-cart"
+              className="bi bi-cart"
               viewBox="0 0 16 16"
             >
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
@@ -181,7 +182,7 @@ export default function Carrinho() {
             </Button>
         
           </div>
-          
+
           <EmptyCart />
         </Card>
       </Container>
