257e96a45dfae44a4c217996bc566b888fceb9e3
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-09-27 00:12:31 -0300

    Requests pg Prod.jsx

--- src/pages/prod_pg/prod.jsx
+++ src/pages/prod_pg/prod.jsx
@@ -2,18 +2,32 @@ import 'react-bootstrap/'
 import Container from "react-bootstrap/Container"
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Prod() {
+  const { id } = useParams();
+  console.log(id);
+  const [prods, definirProds] = useState([]);
+  useEffect(() => {
+    fetch(`http://52.53.150.144:8081/products/${id}`)
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json();
+      })
+      .then((resJson) => {
+        definirProds(resJson)
+        //console.log(resJson)
+      });
+  }, []);
   return (
     <>
       <Container className="mt-3 mb-4">
-        <h1>request: categoryName, brand, title</h1>
-        <hr />
+        <h1>{prods.brand} {prods.title}</h1>
+        <hr/>
         <Card>
-          <Card.Img variant="top" src="https://www.absoluteincorporated.com/wp-content/uploads/2017/09/700x500.png" />
+          <Card.Img variant="top" src={prods.image} />
           <Card.Body>
-            <Card.Title>R$: Price request</Card.Title>
+            <Card.Title>R$: {prods.price}</Card.Title>
             <div className='d-flex align-content-center justify-content-around' style={{ width: '9rem' }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-truck" viewBox="0 0 16 16"
                 style={{ margin: '5px', color: 'yellowgreen' }}>
@@ -22,7 +36,7 @@ export default function Prod() {
               <p style={{ color: 'yellowgreen' }}>Free delivery</p>
             </div>
             <Link className="d-flex justify-content-between" to="/pg_carrinho" style={{ width: '9rem', color: 'yellowgreen' }}>
-              <Button className="d-flex justify-content-between" style={{ width: '9rem', color: '#fff', background: 'yellowgreen', border: 'thin' }}>
+              <Button className="d-flex justify-content-between" style={{ width: '9rem', color: '#fff', background: 'yellowgreen', border: 'thin', height: '35px' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag-check-fill" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
                 </svg>
@@ -32,14 +46,12 @@ export default function Prod() {
             <hr />
             <Card.Text>
               <Card.Title>Description:</Card.Title>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              {prods.description}
             </Card.Text>
           </Card.Body>
         </Card>
       </Container>
     </>
-    //Perdão pelos brs assinado por:felce kkk
   );
 }
 
